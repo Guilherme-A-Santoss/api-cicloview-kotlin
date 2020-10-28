97bb7ce118b41e1c29d0274acdeb668920e12f12
Guilherme-A-Santos <guilhermealves3489@hotmail.com>
2020-10-28 01:26:09 -0300

    Refatorando código para peças

--- api_flask_cicloview.py
+++ api_flask_cicloview.py
@@ -5,94 +5,99 @@ import random
 
 app = Flask(__name__)
 
-bicicletas = [
+pecas = [
     {
         "id": 1,
-        "nome": "Bicicleta Mountain",
-        "tipo": "Manutenção",
-        "imagem": "https://images.unsplash.com/photo-1572111504021-40abd3479ddb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80",
-        "Proprietário": "Kauê"
+        "nome": "Selim em gel",
+        "marca": "Shimano",
+        "imagem": "https://http2.mlstatic.com/D_NQ_NP_628501-MLB43189727805_082020-O.webp",
+        "codigo": "C2904L1"
+        "valor": 71.90
     },
     {
         "id": 2,
-        "nome": "Bicicleta de trilha",
-        "tipo": "Manutenção",
-        "imagem": "https://images.unsplash.com/photo-1566480047210-b10eaa1f8095?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
-        "Proprietário": "Lucas Rangel"
+        "nome": "Catraca Roda Livre Bike 18 Dentes Esferada Dourada",
+        "marca": "Paco",
+        "imagem": "https://http2.mlstatic.com/D_NQ_NP_781848-MLB43105592528_082020-O.webp",
+        "codigo": "C3934D2"
+        "valor": 23.90
     },
     {
         "id": 3,
-        "nome": "Bicicleta Speed",
-        "tipo": "Pintura",
-        "imagem": "https://images.unsplash.com/photo-1532298229144-0ec0c57515c7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1308&q=80",
-        "Proprietário": "Guilherme M."
+        "nome": "Câmbio Traseiro Com Gancheira 7 Velocidades Index P/bike Mtb",
+        "marca": "Yamada",
+        "imagem": "https://http2.mlstatic.com/D_NQ_NP_994456-MLB43473857848_092020-O.webp",
+        "codigo": "S2414W6"
+        "valor": 33.90
     },
     {
         "id": 4,
-        "nome": "Bicicleta Mountain",
-        "tipo": "Manutenção",
-        "imagem": "https://images.unsplash.com/photo-1530173235220-f6825c107a77?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1267&q=80",
-        "Proprietário": "Fernando Henrique"
+        "nome": "Par Pneu Pirelli Speed 700x25 Tornado + 2 Camara+ 2 Fita A/f",
+        "marca": "Pirelli",
+        "imagem": "https://http2.mlstatic.com/D_NQ_NP_920616-MLB31778384975_082019-O.webp",
+        "codigo": "J4546P9"
+        "valor": 189.90
     },
     {
         "id": 5,
-        "nome": "Bicicleta Mountain",
-        "tipo": "Manutenção",
-        "imagem": "https://images.unsplash.com/photo-1575585269294-7d28dd912db8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
-        "Proprietário": "Paulo Castro"
+        "nome": "Quadro Bike Mtb Aro 29 Absolute Nero Alumínio Disc Amarelo",
+        "marca": "Absolute",
+        "imagem": "https://http2.mlstatic.com/D_NQ_NP_600911-MLB43846632034_102020-O.webp",
+        "codigo": "C4565Q2"
+        "valor": 599.49
     },
 ]
 
-@app.route("/bicicletas", methods=['GET'])
+@app.route("/pecas", methods=['GET'])
 def get():
-    return jsonify(bicicletas)
+    return jsonify(pecas)
 
-@app.route("/bicicletas/<int:id>", methods=['GET'])
+@app.route("/pecas/<int:id>", methods=['GET'])
 def get_one(id):
-    filtro = [e for e in bicicletas if e["id"] == id]
+    filtro = [e for e in pecas if e["id"] == id]
     if filtro:
         return jsonify(filtro[0])
     else:
         return jsonify({})
 
-@app.route("/bicicletas", methods=['POST'])
+@app.route("/pecas", methods=['POST'])
 def post():
-    global bicicletas
+    global pecas
     try:
         content = request.get_json()
 
         # gerar id
-        ids = [e["id"] for e in bicicletas]
+        ids = [e["id"] for e in pecas]
         if ids:
             nid = max(ids) + 1
         else:
             nid = 1
         content["id"] = nid
-        bicicletas.append(content)
-        return jsonify({"status":"OK", "msg":"Bicicleta adicionada a fila com sucesso"})
+        pecas.append(content)
+        return jsonify({"status":"OK", "msg":"Peça adicionada do estoque com sucesso"})
     except Exception as ex:
         return jsonify({"status":"ERRO", "msg":str(ex)})
 
-@app.route("/bicicletas/<int:id>", methods=['DELETE'])
+@app.route("/pecas/<int:id>", methods=['DELETE'])
 def delete(id):
-    global bicicletas
+    global pecas
     try:
-        bicicletas = [e for e in bicicletas if e["id"] != id]
-        return jsonify({"status":"OK", "msg":"Bicicleta removida da fila com sucesso"})
+        pecas = [e for e in pecas if e["id"] != id]
+        return jsonify({"status":"OK", "msg":"Peça removida do estoque com sucesso"})
     except Exception as ex:
         return jsonify({"status":"ERRO", "msg":str(ex)})
 
 @app.route("/push/<string:key>/<string:token>", methods=['GET'])
 def push(key, token):
-	b = random.choice(bicicletas)
+	b = random.choice(pecas)
 	data = {
 		"to": token,
 		"notification" : {
 			"title":b["nome"],
-			"body":"Você tem nova ordem de serviço em "+b['nome']
+			"body":"Você tem nova peca registrada"+b['nome']
 		},
 		"data" : {
-			"biscicletaId":b['id']
+			"pecaId":b['id']
 		}
 	}
 	req = urllib.request.Request('http://fcm.googleapis.com/fcm/send')
